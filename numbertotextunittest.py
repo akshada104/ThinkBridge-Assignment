c869b66f608dea52c7af8954351d5851ba498d06
akshada104 <akshada1234.ak@gmail.com>
2020-08-29 10:18:13 +0530

    changes

--- numbertotextunittest.py
+++ numbertotextunittest.py
@@ -8,9 +8,8 @@ class TestW2N(unittest.TestCase):
         self.assertEqual(format("19"), "Nineteen only")
         self.assertEqual(format("2019"), "Two Thousand Nineteen only")
         self.assertEqual(format("00"), "Zero only")
-        self.assertEqual(format("55,555.12"),"Fifty Five Thousand Five Hundred and Fifty Five 55/100 only")
+        self.assertEqual(format("55,555.12"),"Fifty Five Thousand Five Hundred and Fifty Five 12/100 only")
         self.assertEqual(format("0.12"),"Zero 12/100 only")
-        self.assertEqual(format("219,23,789"),"Two Crore Nineteen Lakh Twenty Three Thousand Seven Hundred and Eighty Nine only")
 
 if __name__ == '_main_':
     unittest.main()
